094862fc93c9ca6af5fc6ad6f8fd7c359e285935
Marijke Stein <marijke.stein@gmx.de>
2025-12-20 14:28:37 +0100

    added host 'cookie'

---
+++ hosts/cookie/configuration.nix
@@ -0,0 +1,255 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+  fileSystems."/pub" = {
+    device = "192.168.0.250:/Backup";
+    fsType = "nfs";
+  };
+  boot.supportedFilesystems = [ "nfs" ];
+
+
+  programs.bash.shellAliases = {
+    la = "eza -ahl";
+  };
+
+  networking.hostName = "cookie"; # Define your hostname.
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "de_DE.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the XFCE Desktop Environment.
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "de";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "de";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+  services.printing.drivers = [ pkgs.cups-filters pkgs.gutenprint ];
+  
+  hardware.printers = {
+    ensureDefaultPrinter = "Brother9570";
+    ensurePrinters = [
+      {
+        name = "Brother9570";
+        description = "Brother MFC-L9570CDW";
+        deviceUri = "ipp://192.168.0.100:631/ipp/print";
+        model = "everywhere";
+        ppdOptions = {
+          PageSize = "A4";
+        };
+      }
+    ];
+  };
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+  
+  users.groups.family.gid = 2020;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.marijke = {
+    uid = 2020;
+    group = "family";
+    isNormalUser = true;
+    description = "Marijke Stein";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  users.users.bieni = {
+    uid = 1980;
+    group = "family";
+    isNormalUser = true;
+    description = "Sabine Stein";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  users.users.caro = {
+    uid = 2008;
+    group = "family";
+    isNormalUser = true;
+    description = "Carolin Stein";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    bat
+    bottom
+    curl
+    eza
+    file
+    git
+    gnupg
+    htop
+    killall
+    mc
+    mmv
+    pciutils
+    tree
+    unzip
+    usbutils
+    wget
+    zip
+
+    bluez
+    eog
+    evince
+    gimp
+    gnome-terminal
+    keepassxc
+    mate.mate-calc
+    mtpfs
+    pinentry-gtk2
+    signal-desktop
+    system-config-printer
+    thunderbird
+    totem
+    vlc
+    xdg-desktop-portal-gtk              # e.g. Gtk FileChooser used by various tools
+    xfce.thunar-volman
+
+    libsForQt5.qt5ct
+    kdePackages.kate
+    kdePackages.kconfig
+    kdePackages.konsole
+    kdePackages.okular
+    marksman
+
+    pkgs.backintime-qt
+    pkgs.cron
+    pkgs.fcron
+
+    #corefonts
+    #vistafonts
+
+    hyphen
+    hyphenDicts.de_DE
+    hyphenDicts.de-de
+    libreoffice
+
+    texliveTeTeX
+    gnumake
+
+    nmap
+    wirelesstools
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  services.gvfs.enable = true;
+
+  # Flatpak:
+  xdg.portal.enable = true;
+  services.flatpak.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-xapp ];
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
---
+++ users/marijke/home-cookie.nix
@@ -0,0 +1,191 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "marijke";
+  home.homeDirectory = "/home/marijke";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+
+    pkgs.home-manager
+
+    pkgs.borgbackup
+    pkgs.espanso
+    pkgs.xfce.xfconf
+
+    pkgs.libsForQt5.qt5ct
+    pkgs.kdePackages.kate
+    pkgs.kdePackages.kconfig
+    pkgs.kdePackages.konsole
+    pkgs.kdePackages.okular
+    pkgs.marksman
+
+    #pkgs.corefonts
+    #pkgs.vistafonts
+
+    pkgs.cdparanoia
+    pkgs.flac
+    pkgs.vorbis-tools
+
+    #pkgs.fluffychat
+
+    pkgs.gitkraken
+    pkgs.jetbrains.pycharm-community-bin
+    pkgs.python313
+    pkgs.python313Packages.ipython
+    pkgs.rustup
+    pkgs.starship
+    pkgs.uv
+
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.ls file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.sessionVariables = {
+    EDITOR = "mcedit";
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Marijke Stein";
+    userEmail = "marijke.stein@gmx.de";
+    aliases = {
+      co = "checkout";
+      st = "status";
+    };
+    extraConfig = {
+      core.editor = "mcedit";
+    };
+  };
+
+  home.file.".bashrc" = {
+  text = ''
+alias la="eza -l --icons --git"
+
+eval "$(starship init bash)"
+    '';
+  };
+  
+  
+  # Flatpak:
+  #xdg.portal.enable = true;
+  #services.flatpak.enable = true;
+  #xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-xapp ];
+
+  services.gpg-agent.enable = true;
+  services.gpg-agent.pinentry.package = pkgs.pinentry-gtk2;
+
+  gtk.enable = true;
+  gtk.theme.name = "Adwaita-dark";
+
+  qt.enable = true;
+  qt.style.name = "darker";
+
+  xfconf = {
+    settings = {
+      xfce4-desktop = {
+        "backdrop/screen0/monitoreDP-1/workspace0/image-style" = 0;
+        "desktop-icons/file-icons/show-filesystem" = 0;
+        "desktop-icons/file-icons/show-trash" = 1;
+      };
+
+      xfce4-notifyd = {
+        "mute-sounds" = true;
+      };
+
+      xfce4-panel = {
+        "panels/dark-mode" = true;
+        "panels/panel-1/icon-size" = 12;
+        "panels/panel-1/output-name" = "eDP-1";
+        "panels/panel-1/size" = 24;
+
+        "plugin-1" = "applicationsmenu";
+
+        "plugin-2" = "tasklist";
+        "plugin-2/grouping" = 0;
+
+        "plugin-3" = "separator";
+        "plugin-3/expand" = true;
+        "plugin-3/style" = 0;
+
+        "plugin-4" = "pager";
+
+        "plugin-5" = "separator";
+
+        "plugin-6" = "systray";
+
+        "plugin-7" = "separator";
+
+        "plugin-8" = "clock";
+        "plugin-8/digital-date-font" = "Sans 10";
+        "plugin-8/timezone" = "Europe/Berlin";
+
+        "plugin-9" = "separator";
+
+        "plugin-10" = "actions";
+
+        "plugin-11" = "power-manager-plugin";
+
+        "plugin-13" = "clock";
+        "plugin-13/digital-layout" = 3;
+        "plugin-13/digital-time-font" = "Sans 10";
+      };
+
+      xfce4-screensaver = {
+        "lock/saver-activation/enabled" = false;
+        "saver/enabled" = false;
+      };
+
+      xsettings = {
+        "MonospaceFontName" = "Gtk/JetBrainsMono Nerd Font 10";
+        "Net/IconThemeName" = "Adwaita";
+        "Net/ThemeName" = "Adwaita-dark";
+      };
+    };
+  };
+}
