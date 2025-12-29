9df58e6c51173b95206b53ab296460b813a2354d
Marijke Stein <marijke.stein@hfmdk-frankfurt.de>
2025-12-29 09:06:43 +0100

    do not use 'nom' as it has difficulties with interactive FIDO2 prompts

--- nixos-rebuild-switch.sh
+++ nixos-rebuild-switch.sh
@@ -4,15 +4,7 @@ set -uxo pipefail
 
 cd "flakes" || exit 1
 
-which nom &> /dev/null
-export NOM_FOUND=$?
-
-if [[ "${NOM_FOUND}" == "1" ]]
-then
-    nixos-rebuild switch --flake . --use-remote-sudo     # in future release changed to "--sudo"
-else
-    nixos-rebuild switch --flake . --use-remote-sudo 2>&1 | nom
-fi
+nixos-rebuild switch --flake . --use-remote-sudo
 
 
 # EOF
