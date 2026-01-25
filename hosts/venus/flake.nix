4f558ea13e218cb71326a259f09ed0d7c1455a04
Marijke Stein <marijke.stein@honda-ri.de>
2026-01-25 18:47:46 +0100

    venus: added scanner support (SANE)

---
+++ hosts/venus/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }: {
+     nixosConfigurations = {
+       "nixos" = nixpkgs.lib.nixosSystem {
+         system = "x86_64-linux";
+         modules = [ ./configuration.nix
+                     ./bluetooth.nix ];
+       };
+     };
+  };
+}
