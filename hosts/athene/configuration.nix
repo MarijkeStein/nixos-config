09325effa977e71a98643db6e47be78dcb4cf2dd
Marijke Stein <marijke.stein@gmx.de>
2026-05-14 11:53:31 +0200

    added host 'athene'

---
+++ hosts/athene/configuration.nix
@@ -0,0 +1,292 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "nfs" ];
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+  fileSystems."/pub" = {
+    device = "192.168.0.250:/Backup";
+    fsType = "nfs";
+  };
+  fileSystems."/pub/videos" = {
+    device = "192.168.0.250:/Multimedia";
+    fsType = "nfs";
+  };
+
+  programs.bash.shellAliases = {
+    la = "eza -ahl";
+  };
+
+  networking.hostName = "athene"; # Define your hostname.
+  networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Garbage collection
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 90d";
+    persistent = true;
+    randomizedDelaySec = "3h";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "de_DE.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the XFCE Desktop Environment.
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "de";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "de";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+  services.printing.drivers = [ pkgs.cups-filters pkgs.gutenprint ];
+
+  hardware.printers = {
+    ensureDefaultPrinter = "Brother9570";
+    ensurePrinters = [
+      {
+        name = "Brother9570";
+        description = "Brother MFC-L9570CDW";
+        deviceUri = "ipp://192.168.0.100:631/ipp/print";
+        model = "everywhere";
+        ppdOptions = {
+          PageSize = "A4";
+        };
+      }
+    ];
+  };
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    wireplumber.extraConfig = {
+    "10-bluez" = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-sbc-xq" = true;
+        "bluez5.enable-msbc" = true;
+        "bluez5.enable-hw-volume" = true;
+        "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" "a2dp_sink" "a2dp_source" ];
+        };
+      };
+    };
+
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  users.groups.family.gid = 2020;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.marijke = {
+    uid = 2020;
+    group = "family";
+    isNormalUser = true;
+    description = "Marijke Stein";
+    extraGroups = [ "lp" "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  users.users.caro = {
+    uid = 2008;
+    group = "family";
+    isNormalUser = true;
+    description = "Carolin Stein";
+    extraGroups = [ "lp" "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  programs.fish.enable = true;
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    bat
+    bottom
+    curl
+    eza
+    file
+    fish
+    git
+    gnupg
+    htop
+    killall
+    mc
+    mmv
+    nfs-utils
+    ox
+    pciutils
+    tree
+    unzip
+    usbutils
+    yazi
+    wget
+    zellij
+    zip
+
+    backintime-qt
+    bluez
+    eog
+    evince
+    gimp
+    gnome-terminal
+    gnomeExtensions.bluetooth-battery-meter
+    keepassxc
+    mate.mate-calc
+    meld
+    mtpfs
+    pinentry-gtk2
+    pipewire
+    pulseaudio
+    system-config-printer
+    thunderbird
+    totem
+    vlc
+    xdg-desktop-portal-gtk              # e.g. Gtk FileChooser used by various tools
+    xfce.thunar-volman
+    xfce.xfce4-screensaver
+
+    libsForQt5.qt5ct
+    kdePackages.kate
+    kdePackages.kconfig
+    kdePackages.konsole
+    kdePackages.okular
+    marksman
+
+    pkgs.backintime-qt
+    pkgs.cron
+    pkgs.fcron
+
+    #corefonts
+    #vistafonts
+
+    hyphen
+    hyphenDicts.de_DE
+    hyphenDicts.de-de
+    libreoffice
+
+    tex-fmt
+    texliveTeTeX
+    gnumake
+
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  services.blueman.enable = true;
+
+  services.gvfs.enable = true;
+
+  # Flatpak:
+  xdg.portal.enable = true;
+  services.flatpak.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-xapp ];
+
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+    settings.PermitRootLogin = "yes";
+  };
+
+  services.rpcbind.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  system.autoUpgrade = {
+    enable = true;
+    allowReboot = false;
+    dates = "daily";
+    randomizedDelaySec = "30min";
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
